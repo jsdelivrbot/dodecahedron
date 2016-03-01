16ae6b00d0e9bd0ef1652be3050150e91c3b85c6
Paul M. Christian <paulmchristian@gmail.com>
2016-03-01 14:29:26 -0600

    numbered dodecahedron faces, centers, rotate around center of any face

--- src/components/Dodecahedron.js
+++ src/components/Dodecahedron.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {Entity} from 'aframe-react';
-import Sphere from './Sphere'
-import Polygon from './Polygon'
-import Text from './Text'
-
+import Sphere from './Sphere';
+import Polygon from './Polygon';
+import Text from './Text';
+import _ from 'lodash';
 /*
 
  P(± a, ± a, ± a)
@@ -17,7 +18,7 @@ import Text from './Text'
 const phi = (1 + Math.sqrt(5)) / 2;
 const sqrt3 = Math.sqrt(3);
 
-const vertices = r => {
+const getVertices = r => {
   const a = r / sqrt3;
   const b = r / (sqrt3 * phi);
   const c = (r * phi) / sqrt3;
@@ -51,26 +52,57 @@ const acc = [
   [0,12,3,17,16],
   [0,16,2,9,8],
   [1,8,9,4,18],
-  [1,18,19,6,13]
+  [1,18,19,6,13],
+  [3,12,13,6,10],
+  [2,14,15,4,9],
+  [2,16,17,5,14],
+  [3,10,11,5,17],
+  [4,15,7,19,18],
+  [5,11,7,15,14],
+  [6,19,7,11,10]
 ];
+
+const averageVecs = vecs => _.reduce(
+  vecs,
+  (acc, val) => val.add(acc),
+  V3(0, 0, 0)
+).divideScalar(vecs.length);
+
 
 class Dodecahedron extends React.Component {
   render() {
-    const pentagons = acc.map(verts => verts.map(i => vertices(1.2 * this.props.radius)[i]));
+    const vertices = getVertices(1.2 * this.props.radius);
+
+    const pentagons = acc.map(verts => {
+      const pentVerts = verts.map(i => vertices[i]);
+      return {
+        pentVerts,
+        center: averageVecs(pentVerts.map(vert => arrToV3(vert)))
+      };
+
+    });
 
     return (
-      <Entity>
-        {pentagons.map(p => <Polygon vertices={p} />)}
-        {vertices(this.props.radius).map((v, i) =>
+      <Entity ref={(cmp)=> {
+        const el = ReactDOM.findDOMNode(cmp);
+        if(!el) return;
+        const obj = el.object3D;
+        obj.rotateOnAxis(pentagons[8].center.normalize(), 0.05);
+      }}>
+        {pentagons.map(p => <Polygon vertices={p.pentVerts} />)}
+        {pentagons.map((p, i) => <Entity look-at="[camera]" position={`${p.center.x * 0.8} ${p.center.y * 0.8} ${p.center.z * 0.8}`}>
+          <Text text={{text: i, size: 0.2}}  />
+        </Entity> )}
+        {getVertices(this.props.radius).map((v, i) =>
           <Sphere radius={0.1}
                   onMouseEnter={()=>{
-                    if(acc[acc.length - 1].length === 5) {
+                    /*if(acc[acc.length - 1].length === 5) {
                       acc.push([]);
                     }
 
                     let arr = acc[acc.length - 1];
                     arr.push(i);
-                    console.log(JSON.stringify(acc));
+                    console.log(JSON.stringify(acc));*/
                   }}
                   key={i}
                   position={V3(v[0], v[1], v[2]).toAframeString()}
--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 export default props => {
   const {radius, color, src, position, rotation} = props;
+
   return (
     <Entity {...props} geometry={{primitive: 'sphere', radius: props.radius}}
             position={props.position}
